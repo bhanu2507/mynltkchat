f79162b52b21b4d891f623d38c3c98a59455d9cd
bhanu2507 <bhanureddy@gmail.com>
2017-11-17 16:27:30 +0530

    botresponse from api

--- js/controller.js
+++ js/controller.js
@@ -2,17 +2,33 @@
 
 angular.module('mynltkchat')
     .controller('mnc_ctrl',['$scope','mncservice', '$timeout', '$sce', '$firebaseArray', function($scope, mncservice, $timeout, $sce, $firebaseArray) {
-        let ref = firebase.database().ref();
+        
+        /* let ref = firebase.database().ref();
         let list = $firebaseArray(ref);
         list.$loaded().then(function() {
             $scope.list = [];
             angular.forEach(list, function(value,key){
                $scope.list.push({ id: key, data: value})
             })
-            console.log($scope.list[0].data[1]);
-         });
-
+            console.log($scope.list[0]);
+         }); */
+        
         //console.log($scope.list);
+        mncservice.flist(function(list){
+                $scope.list = list[0].data;
+                $scope.bot = "";
+                let tinterval = 2000;
+                for (let i=0;i<list[0].data.length;i++) {
+                    (function(i){
+                        $timeout(function () {
+                            console.log(list[0].data[i]);
+                            $scope.bot = $scope.bot + $sce.trustAsHtml("<bot-talk cline='" + list[0].data[i].botresponse + "'></bot-talk>");
+                        }, i * 2000)
+                    })(i);
+
+                }
+
+            })
 
         $scope.bclick = function() {
             $scope.bot = $scope.bot + $sce.trustAsHtml("<user-talk cline=" + $scope.chatline +"></user-talk>");
@@ -20,7 +36,7 @@ angular.module('mynltkchat')
 
         $scope.optns = [{opt: "Banking"},{opt : "Investment"}, {opt : "Not Sure"}];
 
-        $timeout(function () {
+/*         $timeout(function () {
             $scope.bot = $sce.trustAsHtml("<bot-talk cline='Welcome!!'></bot-talk>");
         }, 2000)
 
@@ -32,5 +48,5 @@ angular.module('mynltkchat')
             //$scope.bot = $scope.bot + "<options cline=optns bot=bot></options>";
             $scope.usrselect = true;
             $scope.soptions = "<options cline=optns bot=bot usrselect=usrselect></options>";
-        }, 5000)
+        }, 5000) */
     }])
--- js/service.js
+++ js/service.js
@@ -1,14 +1,28 @@
 'use strict';
 
 angular.module('mynltkchat')
-    .service('mncservice', ['$resource', function($resource) {
+    .service('mncservice', ['$resource', '$firebaseArray', '$q', function($resource, $firebaseArray, $q) {
         let self = this;
-        this.stest = "hello";
-        this.chattalk = "";
+
         this.mnc_gettags = function(ltext) {
             //let tags = $resource('http://47.89.183.51:1234/gettags:line', {sline:'@line'});
             let tags = $resource('http://47.89.183.51:1234/rasanlu:line', {sline:'@line'});
             let result = tags.get({sline:encodeURIComponent(ltext)});
             return result;
         }
+        
+        let ref = firebase.database().ref();
+        let list = $firebaseArray(ref);
+
+        this.flist = function(callback) {
+            list.$loaded().then(function() {
+                const glist = [];
+                angular.forEach(list, function(value,key){
+                glist.push({ id: key, data: value});
+                }); 
+              callback(glist);
+            })
+        };
+        
+
     }])
